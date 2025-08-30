7db4945d930896dfccd390ec095fe83c5bb4c7b1
SmexiCH <ammannpascal@hotmail.com>
2025-08-30 15:52:43 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='helpdesk-full-app';
+const CACHE='helpdesk-full-appv2';
 const ASSETS=['./','./index.html','./data.json','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));self.clients.claim();});

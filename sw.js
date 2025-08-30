f99cb3b0618a21fa021b4f1f88392a9be8397091
SmexiCH <ammannpascal@hotmail.com>
2025-08-30 19:04:17 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='wikistoeria-glass-v1';
+const CACHE='wikistoeria-glass-v2-fixflow';
 const ASSETS=['./','./index.html','./data.json','./manifest.webmanifest','./icon-192.png','./icon-512.png','./bremsen-auszupfen.png','./pneumatiktafel.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));self.clients.claim();});
